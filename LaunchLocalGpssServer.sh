d2ab6157f6e099577128d70fd63d4d028fb878c6
KY22 <fatulazy@gmail.com>
2026-03-07 22:31:18 -0300

    dll y launcher script

--- LaunchLocalGpssServer.sh
+++ LaunchLocalGpssServer.sh
@@ -2,4 +2,4 @@
 
 #podman build -t localgpss:2.01 .
 
-podman run --rm -it -p 8080:8080 --name LocalGpssServer --volume localgpss:/app/data/ localgpss:2.01
+podman run --rm -it -p 8080:8080 --name LocalGpssServer --volume localgpss:/app/data/ localgpss:2.0.1
