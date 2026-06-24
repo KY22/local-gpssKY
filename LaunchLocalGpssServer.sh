0441e5bd3e80338eb517cd68052bf856a8e3405a
KY22 <fatulazy@gmail.com>
2026-06-24 19:10:52 -0300

    corregido script de creacion del contenedor de podman

--- LaunchLocalGpssServer.sh
+++ LaunchLocalGpssServer.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-#podman build -t localgpss:2.01 .
+#podman build -t localgpss:2.0.1 .
 
-podman run --rm -it -p 8080:8080 --name LocalGpssServer --volume localgpss:/app/data/ localgpss:2.0.1
+# podman run --rm -it -p 8080:8080 --name LocalGpssServer --volume localgpss:/app/data/ localgpss:2.0.1
+podman run -dit -p 22022:22022 --name LocalGpssServer --volume localgpss:/app/data/ localgpss:2.0.1
